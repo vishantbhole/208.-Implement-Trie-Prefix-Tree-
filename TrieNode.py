c7e09d216c7b1f5425531b80d6e671f6c8401ab7
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-12-18 20:39:02 -0600

    Update TrieNode.py

--- TrieNode.py
+++ TrieNode.py
@@ -7,3 +7,11 @@ class TrieNode:
 class Trie:
     def __init__(self):
         self.root = TrieNode()
+        
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.end = True
